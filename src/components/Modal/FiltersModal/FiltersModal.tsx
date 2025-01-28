42e64abdd1eef5610de4fc72692f1baded927d65
ljs981026 <ljs981026@naver.com>
2025-01-28 23:19:58 +0900

    정렬 모달 추가

--- src/components/Modal/FiltersModal/FiltersModal.tsx
+++ src/components/Modal/FiltersModal/FiltersModal.tsx
@@ -1,8 +1,96 @@
+import { FaTimes } from 'react-icons/fa';
+import { useAppDispatch } from '../../../hook/redux';
+import { toggleFiltersModal } from '../../../store/modal/modalSlice';
+import { DeleteBox, FixedContainer } from '../Modal.styles';
+import { Box, Container, TopBox } from './FiltersModal.styles';
 
-const FiltersModal = () => {
-  return (
-    <div>FiltersModal</div>
-  )
+interface FilterModalProps {
+  handleFilter: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleClear: () => void;
+  filter: string;
 }
 
-export default FiltersModal
+const FiltersModal = ({ handleFilter, handleClear, filter }: FilterModalProps) => {
+  const dispatch = useAppDispatch();
+  return (
+    <FixedContainer>
+      <Container>
+        <DeleteBox
+          onClick={() => dispatch(toggleFiltersModal(false))}
+          className="filters__close"
+        >
+          <FaTimes />
+        </DeleteBox>
+        <TopBox>
+          <div className="filters__title">정렬</div>
+          <small onClick={handleClear} className="filters__delete">
+            CLEAR
+          </small>
+        </TopBox>
+        <Box>
+          <div className="filters__subtitle">PRIORITY</div>
+          <div className="filters_check">
+            <input
+              type="radio"
+              name="filter"
+              value="low"
+              id="low"
+              checked={filter === 'low'}
+              onChange={(e) => handleFilter(e)}
+            />
+            <label htmlFor="low"> Low to High</label>
+          </div>
+          <div className="filters_check">
+            <input
+              type="radio"
+              name="filter"
+              value="high"
+              id="high"
+              checked={filter === 'high'}
+              onChange={(e) => handleFilter(e)}
+            />
+            <label htmlFor="high"> High to Low</label>
+          </div>
+        </Box>
+        <Box>
+          <div className="filters__subtitle">DATE</div>
+          <div className="filters_check">
+            <input
+              type="radio"
+              name="filter"
+              value="latest"
+              id="new"
+              checked={filter === 'latest'}
+              onChange={(e) => handleFilter(e)}
+            />
+            <label htmlFor="low"> Sort by Latest</label>
+          </div>
+          <div className="filters_check">
+            <input
+              type="radio"
+              name="filter"
+              value="created"
+              id="created"
+              checked={filter === 'created'}
+              onChange={(e) => handleFilter(e)}
+            />
+            <label htmlFor="low"> Sort by Created</label>
+          </div>
+          <div className="filters_check">
+            <input
+              type="radio"
+              name="filter"
+              value="edited"
+              id="edit"
+              checked={filter === 'edited'}
+              onChange={(e) => handleFilter(e)}
+            />
+            <label htmlFor="low"> Sort by Edited</label>
+          </div>
+        </Box>
+      </Container>
+    </FixedContainer>
+  );
+};
+
+export default FiltersModal;
--- src/pages/AllNotes/AllNotes.tsx
+++ src/pages/AllNotes/AllNotes.tsx
@@ -1,50 +1,62 @@
-import { useState } from "react";
-import { useAppDispatch, useAppSelector } from "../../hook/redux"
-import { ButtonOutline, Container, EmptyMsgBox } from "../../styles/styles";
-import { Box, InputBox, TopBox } from "./AllNotes.styles";
-import { toggleFiltersModal } from "../../store/modal/modalSlice";
-import getAllNotes from "../../utils/getAllNotes";
+import { useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../../hook/redux';
+import { ButtonOutline, Container, EmptyMsgBox } from '../../styles/styles';
+import { Box, InputBox, TopBox } from './AllNotes.styles';
+import { toggleFiltersModal } from '../../store/modal/modalSlice';
+import getAllNotes from '../../utils/getAllNotes';
+import { FiltersModal } from '../../components';
 
 const AllNotes = () => {
-
   const dispatch = useAppDispatch();
   const { mainNotes } = useAppSelector((state) => state.noteList);
+  const { viewFiltersModal } = useAppSelector((state) => state.modal);
   const [filter, setFilter] = useState('');
   const [searchInput, setSearchInput] = useState('');
-
+  const filterHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFilter(e.target.value);
+  };
+  const clearHandler = () => {
+    setFilter('');
+  };
   return (
     <Container>
+      {viewFiltersModal && (
+        <FiltersModal
+          handleFilter={filterHandler}
+          handleClear={clearHandler}
+          filter={filter}
+        />
+      )}
       {mainNotes.length === 0 ? (
-        <EmptyMsgBox>
-          노트가 없습니다.
-        </EmptyMsgBox>
-      ) :
-      (
+        <EmptyMsgBox>노트가 없습니다.</EmptyMsgBox>
+      ) : (
         <>
           <TopBox>
             <InputBox>
-              <input type={"text"}
-              value={searchInput} 
-              onChange={(e) => setSearchInput(e.target.value)} 
-              placeholder="노트의 제목을 입력해주세요."
+              <input
+                type={'text'}
+                value={searchInput}
+                onChange={(e) => setSearchInput(e.target.value)}
+                placeholder="노트의 제목을 입력해주세요."
               />
             </InputBox>
             <div className="notes__filter-btn">
-              <ButtonOutline onClick={() => dispatch(toggleFiltersModal(true))} className="nav__btn">  
-                  <span>정렬</span>
+              <ButtonOutline
+                onClick={() => dispatch(toggleFiltersModal(true))}
+                className="nav__btn"
+              >
+                <span>정렬</span>
               </ButtonOutline>
             </div>
           </TopBox>
           <Box>
             {/* notes */}
-            { getAllNotes(mainNotes, filter)}
+            {getAllNotes(mainNotes, filter)}
           </Box>
         </>
-      )
-      
-    }
+      )}
     </Container>
-  )
-}
+  );
+};
 
-export default AllNotes
+export default AllNotes;
